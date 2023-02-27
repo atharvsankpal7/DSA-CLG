9d5207d723626f97a2a435cc00b2c93565850b29
atharvsankpal7 <atharvsankpal69420@gmail.com>
2023-02-28 00:57:36 +0530

    .

--- external/05_searching_sorting_methods.c
+++ external/05_searching_sorting_methods.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 void print_array(int a[])
 {
     printf("The array after performing operation is -->\n");
@@ -145,10 +146,41 @@ void quick_sort(int low, int high, int a[])
         quick_sort(pindex + 1, high, a);
     }
 }
+bool linear_search(int a[], int key)
+{
+    for (int i = 0; i < 20; i++)
+    {
+        if (a[i] == key)
+            return true;
+    }
+    return false;
+}
+bool binary_search(int a[], int key)
+{
+    bubble_sort(a);
+    int low = 0, high = 20, mid;
+    while (low < high)
+    {
+        mid = (low + high) / 2;
+        if (key == a[mid] || key == a[low])
+        {
+            return true;
+        }
+        else if (key > a[mid])
+        {
+            low = mid + 1;
+        }
+        else if (key < a[mid])
+        {
+            high = mid - 1;
+        }
+    }
+    return false;
+}
 int main()
 {
     int arr[20] = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 4, 5, 6, 1, 2, 3}, ch;
-
+    int key;
     do
     {
         printf("1.Bubble sort\n2.Selection sort\n3.Insertion sort\n4.Merge sort\n5.Quick Sort\n6.Linear Search\n7.Binary Search\n8.Unsort the array\n0.EXIT\n");
@@ -172,6 +204,25 @@ int main()
             quick_sort(0, 20, arr);
             print_array(arr);
             break;
+        case 6:
+
+            printf("Enter the element that you want to search in the array--> ");
+            scanf("%d", &key);
+            if (linear_search(arr, key))
+                printf("The number is present in the array\n");
+            else
+                printf("The number is not present in the array\n");
+            break;
+        case 7:
+
+            printf("Enter the element that you want to search in the array--> ");
+            scanf("%d", &key);
+            if (binary_search(arr, key))
+                printf("The number is present in the array\n");
+            else
+                printf("The number is not present in the array\n");
+            break;
+
         case 8:
             unsort(arr);
             break;
